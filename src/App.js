4805684257915d25a4829e40e5329d01c21b8918
Vanek2002020 <80063102+Vanek2002020@users.noreply.github.com>
2021-12-13 18:43:49 +0300

    hg

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Profile } from "./components/Profile/Profile.jsx";
+import { Statistics } from "./components/Statistics/Statistics.jsx";
+import { FriendList } from "./components/FriendList/FriendList.jsx";
+import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory.jsx";
+import user from "./components/Profile/user.json";
+import statisticalData from "./components/Statistics/statistical-data.json";
+import friends from "./components/FriendList/friends.json";
+import transactions from "./components/TransactionHistory/transactions.json";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
+
+export default App;
